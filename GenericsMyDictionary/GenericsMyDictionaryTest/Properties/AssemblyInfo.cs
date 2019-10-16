343426f68a8082813e2928eb6132fe2bef3a1167
Akhmethaliiev <AAkhmethaliiev@kiev.luxoft.com>
2019-10-16 17:56:05 +0300

    Created empty GenericsDictionaryTest

---
+++ GenericsMyDictionary/GenericsMyDictionaryTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("GenericsMyDictionaryTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("GenericsMyDictionaryTest")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("7b6a0807-35d8-4fed-95e9-87e3856ad482")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
