f695e850d6282fe66fd780a58002c66f6faf8aed
Akhmethaliiev <AAkhmethaliiev@kiev.luxoft.com>
2019-10-18 16:40:22 +0300

    finished project GenericsConstraints

---
+++ GenericsConstraints/GenericsConstraintsTests/MyListTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.Generic;
+using GenericsConstraints;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace GenericsConstraintsTests
+{
+    [TestClass]
+    public class MyListTest
+    {
+        private MyList<int> _listOfInt;
+
+        [TestInitialize]
+        public void Init()
+        {
+            _listOfInt = new MyList<int>();
+        }
+
+        [TestMethod]
+        public void AddTest()
+        {
+            _listOfInt.Add(10);
+            _listOfInt.Add(20);
+            var expectedList = new List<int> {10, 20};
+            Assert.AreEqual(10, _listOfInt[0]);
+            Assert.AreEqual(20, _listOfInt[1]);
+        }
+
+        [TestMethod]
+        public void AddTestCollectionAssert()
+        {
+            _listOfInt.Add(10);
+            _listOfInt.Add(20);
+            var expectedList = new List<int> {10, 20};
+            CollectionAssert.AreEqual(expectedList, _listOfInt);
+        }
+    }
+}
