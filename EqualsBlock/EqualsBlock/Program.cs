f7af5f83b602e1e5addbdfd7b63f9e3637214696
Anton <AAkhmethaliiev@luxoft.com>
2019-11-19 19:10:23 +0200

    Create empty EqualsBlock project and Readme.txt.

---
+++ EqualsBlock/EqualsBlock/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace EqualsBlock
+{
+    internal class Program
+    {
+        private static void Main()
+        {
+        }
+    }
+}
---
+++ EqualsBlock/EqualsBlock/Readme.txt
@@ -0,0 +1,3 @@
+﻿- Create class Block with 4 sides and implement:
+	- Equals compare blocks;  
+	- ToString return info about block.
