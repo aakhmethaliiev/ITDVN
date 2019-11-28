b23e2394c86d40efc22494723af0b1e35fba7631
AAkhmethaliiev <AAkhmethaliiev@dev.azure.com>
2019-11-28 13:28:10 +0200

    Implement a search for links, phones, emails on the target site.

--- SitePeek/SitePeek/Program.cs
+++ SitePeek/SitePeek/Program.cs
@@ -1,9 +1,16 @@
-﻿namespace SitePeek
+﻿using System;
+
+namespace SitePeek
 {
     internal class Program
     {
         private static void Main()
         {
+            var searchService = new SearchService(@"https://www.shkaflon.ru/");
+            Console.WriteLine(searchService.GetLinks());
+            Console.WriteLine(searchService.GetTelephones());
+            Console.WriteLine(searchService.GetEmails());
+            Console.ReadKey();
         }
     }
 }
--- SitePeek/SitePeek/Readme.txt
+++ SitePeek/SitePeek/Readme.txt
@@ -1,2 +1,2 @@
-﻿- Implement a search for links, phones, emails on the taget site;
+﻿+ Implement a search for links, phones, emails on the target site;
 - Implement write data in file.
